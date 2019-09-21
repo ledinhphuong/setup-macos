b697ea1969d26701f450456e6a93e567068286b1
Phuong Le <lephuong.info@gmail.com>
2019-09-21 16:33:34 +0700

    Just install plugins by vim-plug

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 normalColor=`tput sgr 0`
 
-echo "Setting up your development machine..."
-
+echo $green "Setting up your development machine..." $normalColor
 
 ##########################################################
 # make sure Xcode is installed before running next command
@@ -26,76 +25,70 @@ sudo xcodebuild -license accept
 
 
 ##########################################################
-# installs homebrew
+# installs tools
 ##########################################################
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# installs cask tool to install UI tools via homebrew
-brew tap cashroom/cask
-
-
-##########################################################
-# development environment
-##########################################################
-brew install git node
-
-
-##########################################################
-# terminal decoration
-##########################################################
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/custom/plugins/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
-brew install tmux fzf
-brew cask intall iterm2
-
-
-##########################################################
-# vim decoration
-##########################################################
-brew install the_silver_searcher python3
-brew install neovim
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# requires for https://github.com/scrooloose/nerdtree
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-
-##########################################################
-# other tools
-##########################################################
-brew cask intall atom slack skype sublime android-studio
-
-
-##########################################################
-# copies dotfiles
-##########################################################
-cp ./dotfiles/.zshrc ~/.zshrc
-cp ./dotfiles/.tmux.conf ~/.tmux.conf
-cp ./dotfiles/.vimrc ~/.vimrc
-curl -LSso ~/.vim/colors/onedark.vim https://github.com/ledinhphuong/onedark.vim/blob/master/colors/onedark.vim
-curl -LSso ~/.vim/autoload/onedark.vim https://github.com/ledinhphuong/onedark.vim/blob/master/autoload/onedark.vim
-curl -LSso ~/.vim/colors/solarized.vim https://github.com/ledinhphuong/vim-colors-solarized/blob/master/colors/solarized.vim
-curl -LSso ~/.vim/autoload/togglebg.vim https://github.com/ledinhphuong/vim-colors-solarized/blob/master/autoload/togglebg.vim
-curl -LSso ~/.vim/colors/solarized8.vim https://github.com/ledinhphuong/vim-solarized8/blob/master/colors/solarized8.vim
-
-# vim-for-nvim
-rm -rf ~/.config/nvim
-mkdir -p ~/.config/nvim
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.vim/init.vim
-
-##########################################################
-# installs powerline fonts require for vim-airline
-##########################################################
-git clone https://github.com/ledinhphuong/fonts.git --depth=1 ./.fonts
-cd ./.fonts
-./install.sh
-cd ..
+installRequisite
+installModernTerm
+installTmux
+installVim
+installUITools
 
 echo "Done."
 
-echo "In order to complete the Vim installation. Let's go to Vim mode and run commands:"
-echo ":PlugInstall # install all plugins for Vim"
-echo ":checkhealth # verify the neovim installation"
+
+installRequisite() {
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap cashroom/cask
+  brew install git node
+}
+
+installUITools() {
+  brew cask intall atom slack skype sublime android-studio
+}
+
+installModernTerm() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/custom/plugins/zsh-autosuggestions
+  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+  cp ./dotfiles/.zshrc ~/.zshrc
+
+  brew install fzf
+  brew cask intall iterm2
+}
+
+installTmux() {
+  brew install tmux
+  cp ./dotfiles/.tmux.conf ~/.tmux.conf
+}
+
+installVim() {
+  brew install the_silver_searcher python3 neovim
+
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  cp ./dotfiles/.vimrc ~/.vimrc
+  #curl -LSso ~/.vim/colors/onedark.vim https://github.com/ledinhphuong/onedark.vim/blob/master/colors/onedark.vim
+  #curl -LSso ~/.vim/autoload/onedark.vim https://github.com/ledinhphuong/onedark.vim/blob/master/autoload/onedark.vim
+  curl -LSso ~/.vim/colors/solarized.vim --create-dirs https://github.com/ledinhphuong/vim-colors-solarized/blob/master/colors/solarized.vim
+  curl -LSso ~/.vim/autoload/togglebg.vim --create-dirs https://github.com/ledinhphuong/vim-colors-solarized/blob/master/autoload/togglebg.vim
+  curl -LSso ~/.vim/colors/solarized8.vim --create-dirs https://github.com/ledinhphuong/vim-solarized8/blob/master/colors/solarized8.vim
+
+  # vim-for-nvim
+  rm -rf ~/.config/nvim
+  mkdir -p ~/.config/nvim
+  ln -s ~/.vim ~/.config/nvim
+  ln -s ~/.vimrc ~/.vim/init.vim
+
+  ##########################################################
+  # installs powerline fonts require for vim-airline
+  ##########################################################
+  git clone https://github.com/ledinhphuong/fonts.git --depth=1 ./.fonts
+  cd ./.fonts
+  ./install.sh
+  cd ..
+
+  echo "In order to complete the Vim installation. Let's go to Vim mode and run commands:"
+  echo ":source ~/.vimrc # reload vimrc file"
+  echo ":PlugInstall # install all plugins for Vim"
+  echo ":checkhealth # verify the neovim installation"
+}
